02c72f6aaa92bf04533e72799074556331df4447
Sarthak Pani <realsarthutrap1@users.noreply.github.com>
2026-09-08 15:49:36 -0700

    lab7: switch reverseArray to pointers

--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@ int main() {
 
 string* reverseArray(string *arr, int size) {
     for (int i = 0; i < size / 2; i++) {
-        string temp = arr[i];
-        arr[i] = arr[size - 1 - i];
-        arr[size - 1 - i] = temp;
+        string temp = *(arr + i);
+        *(arr + i) = *(arr + size - 1 - i);
+        *(arr + size - 1 - i) = temp;
     }
     return arr;
 }
