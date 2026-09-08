e5c15c2aab0fb682be46a977b6a84ff59f72b273
Sarthak Pani <realsarthutrap1@users.noreply.github.com>
2026-09-08 15:00:44 -0700

    lab7: add function stubs

--- main.cpp
+++ main.cpp
@@ -5,8 +5,18 @@ using namespace std;
 
 const int SIZE = 5;
 
+string* reverseArray(string *arr, int size);
+void displayArray(string *arr, int size);
+
 int main() {
     string *arr = new string[SIZE];
 
     return 0;
+}
+
+string* reverseArray(string *arr, int size) {
+    return arr;
+}
+
+void displayArray(string *arr, int size) {
 }
