ea3c0ac6fe02ba2dc8564efec23447c9a3dd42b6
Sarthak Pani <realsarthutrap1@users.noreply.github.com>
2026-09-08 15:09:12 -0700

    lab7: put in the five names

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@ void displayArray(string *arr, int size);
 int main() {
     string *arr = new string[SIZE];
 
+    *(arr + 0) = "Janet";
+    *(arr + 1) = "Jeffe";
+    *(arr + 2) = "Jin";
+    *(arr + 3) = "Joe";
+    *(arr + 4) = "Junio";
+
     return 0;
 }
 
